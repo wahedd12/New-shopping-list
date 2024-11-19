991e50887e26418fba56dbcc29b485906bab8603
Waheed Akinrele <Waheedakinrele@yahoo.com>
2024-11-19 14:21:22 +0100

    connected mongodb and added to .env

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+
 
 const app = express()
 const port = 3000
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB).then(()=>{
+    console.log("mongoose connected")
+})
 
 app.listen(port, ()=>{
     console.log("seerver on 3000")
