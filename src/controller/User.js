8f2ac6867896dee2c05fe10cf4b0f7eaefdd946f
Waheed Akinrele <Waheedakinrele@yahoo.com>
2024-11-27 21:13:18 +0100

    Mail verification added

--- src/controller/User.js
+++ src/controller/User.js
@@ -1,6 +1,10 @@
 const bcrypt = require("bcrypt")
+const nodemailer = require("nodemailer")
+const dotenv = require("dotenv")
 const User = require("../model/User")
 const saltRounds = 10
+
+dotenv.config()
 
 let createUser = async (req, res)=>{
     try {
@@ -16,12 +20,47 @@ let createUser = async (req, res)=>{
      address,
      phoneNumber,
         })
+
+// function for sending mail
+ sendVerificationEmail(newUser)
+
         await newUser.save()
         res.status(201).json(newUser)
     } catch (error) {
         console.error(error)
         res.status(500).json({message: "internal server error"})
     }
+}
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "lmipo861@gmail.com",
+        pass: process.env.MAIL_PASSWORD
+    },
+})
+
+let sendVerificationEmail = async (user) => {
+const verificationLink = `http://localhost:3000/user/verifyUser?token=${user.verificationToken}`
+
+const mailOptions = {
+    from: "Shopping-App <lmipo861@gmail.com>",
+    to: user.email,
+    subject: "verify your email address",
+    text: `
+    Hello${user.name},
+pLease click on the link below to verify your email address: ${verificationLink},
+If you didnt create an account, please disregard this email
+Thank you,
+The Shopping App team.
+    `
+}
+try {
+    transporter.sendMail(mailOptions)
+    console.log("verification sent successfully")
+} catch (error) {
+    console.error(error)
+}
 }
 
 module.exports = { createUser, }
